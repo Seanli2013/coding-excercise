8a24666d292c50e48a130ce9c05c5846f589b50f
Seanli2013 <bigbelledu@gmail.com>
2021-04-06 14:23:36 +0800

    Add solution 2 to 7. Reverse Integer

--- leetcode-swift/7-reverse-integer.swift
+++ leetcode-swift/7-reverse-integer.swift
@@ -7,24 +7,52 @@
 //  https://leetcode-cn.com/problems/reverse-integer/
 //
 
-//  [Solution 1] convertion btw Int and String, String reverse
+//  [Solution 2] convertion btw Int and Array
 class Solution {
     func reverse(_ x: Int) -> Int {
         // 边界条件
         if x == -2147483648 {return 0;}
-
+        
         var x = x
         var sign = 1
         if x < 0 {
             sign = -1
             x = -1 * x
         }
-
-        var myString = String(x)
-        myString = String(myString.reversed())
-
-        let retValue = Int(myString)! * sign
+        var y = 0
+        var tmpNum = x % 10
+        while x > 0 {
+            y = y * 10 + tmpNum
+            x = x / 10
+            tmpNum = x % 10
+        }
+        
+        let retValue = y * sign
         if retValue > 2147483647 || retValue < -2147483648 {return 0}
         return retValue
     }
 }
+
+
+
+//  [Solution 1] convertion btw Int and String, String reverse
+//class Solution {
+//    func reverse(_ x: Int) -> Int {
+//        // 边界条件
+//        if x == -2147483648 {return 0;}
+//
+//        var x = x
+//        var sign = 1
+//        if x < 0 {
+//            sign = -1
+//            x = -1 * x
+//        }
+//
+//        var myString = String(x)
+//        myString = String(myString.reversed())
+//
+//        let retValue = Int(myString)! * sign
+//        if retValue > 2147483647 || retValue < -2147483648 {return 0}
+//        return retValue
+//    }
+//}

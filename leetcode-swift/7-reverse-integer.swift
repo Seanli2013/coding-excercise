aadc996e8adc0ebb3aa6045bdbed8b15610373bc
Seanli2013 <bigbelledu@gmail.com>
2021-04-06 14:02:03 +0800

    Add solution to 7. Reverse Integer

---
+++ leetcode-swift/7-reverse-integer.swift
@@ -0,0 +1,30 @@
+//
+//  7-reverse-integer.swift
+//  
+//
+//  Created by XiangLI on 4/6/21.
+//
+//  https://leetcode-cn.com/problems/reverse-integer/
+//
+
+//  [Solution 1] convertion btw Int and String, String reverse
+class Solution {
+    func reverse(_ x: Int) -> Int {
+        // 边界条件
+        if x == -2147483648 {return 0;}
+
+        var x = x
+        var sign = 1
+        if x < 0 {
+            sign = -1
+            x = -1 * x
+        }
+
+        var myString = String(x)
+        myString = String(myString.reversed())
+
+        let retValue = Int(myString)! * sign
+        if retValue > 2147483647 || retValue < -2147483648 {return 0}
+        return retValue
+    }
+}

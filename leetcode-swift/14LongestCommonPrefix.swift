fb5683879bc89a7207e62a12f04e03afa2aabdcd
Seanli2013 <bigbelledu@gmail.com>
2021-04-07 17:47:51 +0800

    Add another solution to 14. Longest Common Prefix

--- leetcode-swift/14LongestCommonPrefix.swift
+++ leetcode-swift/14LongestCommonPrefix.swift
@@ -10,6 +10,27 @@
 
 import Foundation
 
+// Sort first, then compare the first one and the last one
+// TC: O(n), SC: O(n)
+class LongestCommonPrefixSolution2 {
+    func longestCommonPrefix(_ strs: [String]) -> String {
+        if strs.count == 0 { return "" }
+        if strs.count == 1 { return strs[0] }
+        
+        var strArray = strs
+        strArray.sort()
+        let s1 = strArray[0], s2 = strArray.last!
+        let s = s1.count > s2.count ? s2 : s1
+        for i in 0..<s.count {
+            if s1[s1.index(s1.startIndex, offsetBy: i)] != s2[s2.index(s2.startIndex, offsetBy: i)] {
+                return String(s1.prefix(upTo: s1.index(s1.startIndex, offsetBy: i)))
+            }
+        }
+        return s
+    }
+}
+
+
 // TC: O(nm), SC: O(1)
 class LongestCommonPrefixSolution1 {
     func longestCommonPrefix(_ strs: [String]) -> String {

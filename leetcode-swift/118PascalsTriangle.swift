cea2b2846311b3869e2abe54b1f38b44840917b7
Seanli2013 <bigbelledu@gmail.com>
2021-04-12 16:05:37 +0800

    Add solution to 118. Pascal's Triangle

---
+++ leetcode-swift/118PascalsTriangle.swift
@@ -0,0 +1,45 @@
+//
+//  118PascalsTriangle.swift
+//  CodeExcercise
+//
+//  Created by XiangLI on 4/12/21.
+//
+//
+//  https://leetcode-cn.com/problems/pascals-triangle/
+//
+import Foundation
+class PascalsTriangleSolution1 {
+    func generate(_ numRows: Int) -> [[Int]] {
+        if numRows == 0 { return [] }
+        if numRows == 1 { return [[1]] }
+        
+        var retArray = [[1]], tmpArray = [1]
+        for i in 2...numRows {
+            tmpArray = generateLastArray(tmpArray, i)
+            retArray.append(tmpArray)
+        }
+        return retArray
+    }
+
+    func generateLastArray(_ array: [Int], _ numRows: Int) -> [Int] {
+        if numRows == 1 { return [1] }
+        if array.count != numRows - 1 {
+            // throw something
+            print("array.count: \(array.count); numRows: \(numRows) \n")
+            return []
+        }
+        var retArray = [1]
+        var tmpNum = 0
+        for i in 1..<numRows {
+            if i == array.count {
+                tmpNum = array[i-1]
+            }
+            else {
+                tmpNum = array[i] + array[i - 1]
+            }
+            retArray.append(tmpNum)
+        }
+        // print("retArray: \(retArray)")
+        return retArray
+    }
+}

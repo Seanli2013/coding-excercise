5f8a3d1d4c884ddfdba8e58d85642675978a7375
Seanli2013 <bigbelledu@gmail.com>
2021-04-12 16:23:28 +0800

    Add solution to 509. Fibonacci Number

---
+++ leetcode-swift/509FibonacciNumber.swift
@@ -0,0 +1,36 @@
+//
+//  509FibonacciNumber.swift
+//  CodeExcercise
+//
+//  Created by XiangLI on 4/12/21.
+//
+//  https://leetcode-cn.com/problems/fibonacci-number/
+//
+
+import Foundation
+
+// TC: O(n), SC:O(1)
+class FibonacciNumberSolution2 {
+    func fib(_ n: Int) -> Int {
+        if 0 == n { return 0 }
+        if 1 == n { return 1 }
+
+        // n >= 2
+        var nMinus1 = 1, nMinus2 = 0, retV = 1
+        for _ in 2...n {
+            retV = nMinus1 + nMinus2
+            nMinus2 = nMinus1
+            nMinus1 = retV
+        }
+        return retV
+    }
+}
+
+// TC: O(2^n)
+class FibonacciNumberSolution1 {
+    func fib(_ n: Int) -> Int {
+        // 递归最简单，但复杂度高
+        return fib(n - 1) + fib (n - 2)
+    }
+}
+
